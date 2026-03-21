8b533fa99a86df3f9812fbcf21d4e9ff51d2c7c7
livingsarkwah <159113494+livingsarkwah@users.noreply.github.com>
2026-03-21 15:14:24 +0000

    All done. Minor UI tweaks remaining

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-// https://www.thecolorapi.com/scheme?hex=0047AB&rgb=0,71,171&hsl=215,100%,34%&cmyk=100,58,0,33&format=html&mode=analogic&count=6
+getColorScheme("000000", "monochrome")
 
+const form = document.getElementById("user-form")
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    console.log("Form submitted")
+    let colorInput = document.getElementById("color-input").value
+    colorInput = colorInput.slice(1)
+    const modeInput = document.getElementById("mode-input").value
+    getColorScheme(colorInput, modeInput)
+})
 
-fetch("https://www.thecolorapi.com/scheme?hex=24B1E0")
+function getColorScheme(colorInput, modeInput)  {
+    fetch(`https://www.thecolorapi.com/scheme?hex=${colorInput})&mode=${modeInput}`)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        console.log(data.colors)
+        render(data)
+    })
     .catch(error => console.error("Fetch failed:", error));
+}
+
+function render(data) {
+    let html = ""
+    data.colors.forEach(color => {
+        html += `
+        <div class="color-display">
+            <img class="color"/ src="${color.image.bare}" alt="Color Image">
+            <div class="color-code">
+                ${color.hex.value}
+            </div>
+        </div>`
+    })
+    document.getElementById("color-scheme").innerHTML = html;
+    console.log("Rendering Successful")
+}
